3b61a5519d6d2d6b681c7f847d8583a8aa3e196b
Ugnė Dambrauskė <u.dambrauske@gmail.com>
2024-02-09 16:35:26 +0200

    header component updated, add-todo component updated

---
+++ src/app/container/completed-list/completed-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-completed-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './completed-list.component.html',
+  styleUrl: './completed-list.component.css'
+})
+export class CompletedListComponent {
+
+}
