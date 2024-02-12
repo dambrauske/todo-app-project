cc6e84676640d58e026f549a05d94225580b5806
Ugnė Dambrauskė <u.dambrauske@gmail.com>
2024-02-12 19:14:02 +0200

    style changes

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -2,13 +2,14 @@ import { Component, Input } from '@angular/core';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { AddTodoComponent } from './add-todo/add-todo.component';
 import { TodoItem } from '../models/models';
+import { CompletedListComponent } from "./completed-list/completed-list.component";
 
 @Component({
-  selector: 'app-container',
-  standalone: true,
-  templateUrl: './container.component.html',
-  styleUrl: './container.component.css',
-  imports: [TodoListComponent, AddTodoComponent],
+    selector: 'app-container',
+    standalone: true,
+    templateUrl: './container.component.html',
+    styleUrl: './container.component.css',
+    imports: [TodoListComponent, AddTodoComponent, CompletedListComponent]
 })
 export class ContainerComponent {
   allToDoItems: TodoItem[] = [];
